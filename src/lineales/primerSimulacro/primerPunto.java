59caa08c383e6cce39e0bf8824de8e7dd9bbb7f8
MatiasTarta <129552951+MatiasTarta@users.noreply.github.com>
2023-06-26 19:53:39 -0300

    ya pude solo gracias

--- src/lineales/primerSimulacro/primerPunto.java
+++ src/lineales/primerSimulacro/primerPunto.java
@@ -1,18 +1,13 @@
 package lineales.primerSimulacro;
+
 import lineales.dinamicas.*;
+
 public class primerPunto {
+    public static void main(String[] args) {
+        Lista l = new Lista();
+        l.insertar('A', 1);
 
+        System.out.println("Lista: " + l.toString());
+    }
 
-    l.insertar('A', 1);
-    l.insertar('B', 2);
-    l.insertar('C', 3);
-    l.insertar('D', 4);
-    l.insertar('E', 5);
-    l.insertar('F', 6);
-    l.insertar('G', 7);
-    l.insertar('H', 8);
-    l.insertar('I', 9);
-    l.insertar('J', 10);
-
-    System.out.println("Lista: ")
 }
