6a06bea4713852e0d5e070b882fe66c4cb0ab458
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2025-01-25 12:47:50 -0300

    consultas sobre ciudades

--- src/TPO/Estructuras/TestCarga.java
+++ src/TPO/Estructuras/TestCarga.java
@@ -6,33 +6,35 @@ import java.io.IOException;
 import java.nio.file.Paths;
 
 import TPO.SistemaMudanzas.Ciudad;
-import lineales.dinamicas.Lista;
-import TPO.Estructuras.ArbolAVL;
+
 public class TestCarga {
     public static void main(String[] args) {
         GrafoEtiquetado mapaRutas = new GrafoEtiquetado();
         ArbolAVL diccionario = new ArbolAVL();
         String archivo = Paths.get("src", "TPO", "SistemaMudanzas", "CargaInicial.txt").toString();
-        cargarDatos(archivo,mapaRutas,diccionario);
+        cargarDatos(archivo, mapaRutas, diccionario);
         // System.out.println(mapaRutas.toString());
-        //System.out.println(diccionario.toString()); 
+        System.out.println(diccionario.toString());
+        System.out.println(mostrarInfoCiudad(8332, diccionario));
+
     }
 
-    public static void cargarDatos(String archivo,GrafoEtiquetado mapa,ArbolAVL diccionario){
-        int i=1;
+    public static void cargarDatos(String archivo, GrafoEtiquetado mapa, ArbolAVL diccionario) {
+        int i = 1;
         try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
             String linea;
             while ((linea = br.readLine()) != null) {
                 String[] partes = linea.split(";");
                 switch (partes[0]) {
                     case "C":
-                        cargarCiudad(Integer.parseInt(partes[1]), partes[2], partes[3],diccionario);
+                        cargarCiudad(Integer.parseInt(partes[1]), partes[2], partes[3], diccionario);
                         break;
                     case "R":
-                        insertarRuta( Integer.parseInt(partes[1]),Integer.parseInt(partes[2]),Double.parseDouble(partes[3]), mapa);
+                        insertarRuta(Integer.parseInt(partes[1]), Integer.parseInt(partes[2]),
+                                Double.parseDouble(partes[3]), mapa);
                         break;
                     default:
-                        System.out.println("Formato desconocido en la línea: " + linea+" "+i);
+                        System.out.println("Formato desconocido en la línea: " + linea + " " + i);
                         break;
                 }
                 i++;
@@ -42,28 +44,31 @@ public class TestCarga {
         }
     }
 
-    public static void insertarRuta(int codigoOrigen ,int codigoDestino,  double distancia,GrafoEtiquetado mapa) {
+    public static void insertarRuta(int codigoOrigen, int codigoDestino, double distancia, GrafoEtiquetado mapa) {
         if (!mapa.existeVertice(codigoOrigen)) {
             mapa.insertarVertice(codigoOrigen);
         }
-        
+
         if (!mapa.existeVertice(codigoDestino)) {
             mapa.insertarVertice(codigoDestino);
         }
-        
+
         // Solo agregar el arco si ambos nodos han sido insertados
         if (mapa.existeVertice(codigoOrigen) && mapa.existeVertice(codigoDestino)) {
             mapa.insertarArco(codigoOrigen, codigoDestino, distancia);
         }
     }
 
-    public static void cargarCiudad(int codigo,String nombre,String provincia,ArbolAVL diccionario){
-        Ciudad ciudad= new Ciudad(codigo, nombre, provincia);
-        boolean exito= diccionario.insertar(codigo, ciudad);
+    public static void cargarCiudad(int codigo, String nombre, String provincia, ArbolAVL diccionario) {
+        Ciudad ciudad = new Ciudad(codigo, nombre, provincia);
+        boolean exito = diccionario.insertar(codigo, ciudad);
         if (exito) {
-            
+
         }
     }
 
-    
+    public static String mostrarInfoCiudad(Comparable codigoPostal, ArbolAVL diccionario) {
+        return (diccionario.obtenerDato(codigoPostal).toString());
+    }
+
 }
