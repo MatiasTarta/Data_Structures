aac878f0b06a504162f4837fe2b16fc146bd3bfd
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2025-01-27 18:49:43 -0300

    asociar listo

--- src/TPO/Estructuras/MapeoAMuchos.java
+++ src/TPO/Estructuras/MapeoAMuchos.java
@@ -18,13 +18,7 @@ public class MapeoAMuchos {
         int indice = funcionHash(tipoDocumento, numeroDocumento);
         NodoHashMapeoM actual = arreglo[indice];
         boolean exito=false;
-        if (actual==null) {
-            //si al principio del todo el nodo esta vacio
-            NodoHashMapeoM nuevoNodo = new NodoHashMapeoM(tipoDocumento, numeroDocumento, nombre, apellido, email);
-            arreglo[indice] = nuevoNodo; // Actualizar el arreglo
-            cantidad++;
-            exito=true;
-        }
+        
         // busca si ya existe el dominio (tupla de tipoDocumento y numeroDocumento)
         while (actual != null && !exito) {
             if (actual.getTipoDocumento().equals(tipoDocumento) && actual.getNumeroDocumento() == numeroDocumento) {
@@ -45,7 +39,7 @@ public class MapeoAMuchos {
             if (!exito) {
             //si recorri todos los nodos y no estaba creo un nodo nuevo
             NodoHashMapeoM nuevoNodo = new NodoHashMapeoM(tipoDocumento, numeroDocumento, nombre, apellido, email);
-            nuevoNodo.setEnlace(arreglo[indice]); // Enlazar al inicio de la lista
+            nuevoNodo.setEnlace(arreglo[indice]); // si al principio no habia nada se setea el enlace en nulo
             arreglo[indice] = nuevoNodo; // Actualizar el arreglo
             cantidad++;
             }
