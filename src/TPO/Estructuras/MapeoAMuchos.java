33c8251506f2030c6f5cfcc786be99bb759ce58e
MatiasTarta <matias.tartaglia@est.fi.uncoma.edu.ar>
2025-01-28 11:42:50 -0300

    desacoiar listo

--- src/TPO/Estructuras/MapeoAMuchos.java
+++ src/TPO/Estructuras/MapeoAMuchos.java
@@ -1,4 +1,7 @@
 package TPO.Estructuras;
+
+import lineales.dinamicas.Lista;
+
 public class MapeoAMuchos {
     private int tamanio;
     private NodoHashMapeoM[] arreglo;
@@ -14,7 +17,7 @@ public class MapeoAMuchos {
         return Math.abs((tipoDocumento + numeroDocumento).hashCode()) % tamanio;
     }
 
-    public void asociar(String tipoDocumento, int numeroDocumento, String nombre, String apellido, String email) {
+    public boolean asociar(String tipoDocumento, int numeroDocumento, String nombre, String apellido, String email) {
         int indice = funcionHash(tipoDocumento, numeroDocumento);
         NodoHashMapeoM actual = arreglo[indice];
         boolean exito=false;
@@ -43,6 +46,70 @@ public class MapeoAMuchos {
             arreglo[indice] = nuevoNodo; // Actualizar el arreglo
             cantidad++;
             }
+            return exito;
+    }
+
+    public boolean desacoiar(String tipoDocumento,int numeroDocumento){
+        boolean exito=false;
+        int indice= funcionHash(tipoDocumento, numeroDocumento);
+        NodoHashMapeoM actual=arreglo[indice];
+        NodoHashMapeoM previo=null;
+        while (actual!=null && !exito) {
+            if (actual.getTipoDocumento().equals(tipoDocumento) && actual.getNumeroDocumento() == numeroDocumento){
+                if (previo==null) {
+                    //caso donde el elemento a eliminar esta primero
+                    arreglo[indice]=actual.getEnlace();
+                     exito=true;
+                }else{
+                    //caso donde el elemento a eliminar esta entre medio o al final
+                    previo.setEnlace(actual.getEnlace());
+                    exito=true;
+                }
+            }
+            previo=actual;
+            actual=actual.getEnlace();
+
+        }
+        return exito;
+    }
+
+
+
+    public boolean esVacio() {
+        return cantidad == 0;
+    }
+
+     public Lista obtenerValor(String tipoDocumento, int numeroDocumento) {
+        int indice = funcionHash(tipoDocumento, numeroDocumento);
+        NodoHashMapeoM actual = arreglo[indice];
+        Lista rangos=null;
+        // Buscar el nodo del dominio
+        while (actual != null) {
+            if (actual.getTipoDocumento().equals(tipoDocumento) && actual.getNumeroDocumento() == numeroDocumento) {
+                rangos= actual.getRango();
+            }
+            actual = actual.getEnlace();
+        }
+
+        // Si no se encuentra el dominio, devolver null
+        return rangos;
+    }
+
+    
+
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tamanio; i++) {
+            NodoHashMapeoM actual = arreglo[i];
+            sb.append("Cubeta ").append(i).append(": ");
+            while (actual != null) {
+                sb.append(actual.getTipoDocumento()).append(" -> ").append(actual.getRango()).append(" | ");
+                actual = actual.getEnlace();
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
     
 
